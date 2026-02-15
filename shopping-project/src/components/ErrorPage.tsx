6b092e1669bbd341d0f9e9a4d95ef71b429eee3b
GabrielBRQ <gabrielmedvalle@gmail.com>
2026-02-15 11:11:03 -0300

    remove useState from ErrorPage

--- shopping-project/src/components/ErrorPage.tsx
+++ shopping-project/src/components/ErrorPage.tsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
 import { Link } from "react-router-dom";
 
 function ErrorPage() {
     return (
         <>
-            <h1>Oh no, this route doesn't exist!</h1>
+            <h1>Oh não! Essa rota não existe...</h1>
             <Link to="/">
-                You can go back to the home page by clicking here, though!
+                Você pode voltar para a página clicando aqui
             </Link>
         </>
     )
